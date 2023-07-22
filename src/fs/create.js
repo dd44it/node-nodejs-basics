6053510edb7aea6dcb7e2cad703c26698500a168
dd44it <12nelson13@gmail.com>
2023-07-22 21:15:47 +0300

    feat: done task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,33 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = path.join(folderName, 'create.txt');
+  const contentToWrite = 'I am fresh and young.';
+
+  try {
+    await fs.access(folderName);
+    console.log('Folder "files" already exists.');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      try {
+        await fs.mkdir(folderName);
+        console.log('Folder "files" created.');
+      } catch (err) {
+        console.error('Error creating folder:', err);
+      }
+    } else {
+      console.error('Error accessing folder:', err);
+    }
+  }
+
+  try {
+    await fs.writeFile(fileName, contentToWrite);
+    console.log('File created and text inserted successfully!');
+  } catch (err) {
+    console.error('Error creating/writing the file:', err);
+  }
 };
 
 await create();
