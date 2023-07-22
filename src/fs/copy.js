556c00f4724dfaed563dfe527dcf67228e88aac2
dd44it <12nelson13@gmail.com>
2023-07-22 21:16:03 +0300

    feat: done task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const folderName = 'files'
+const newFolder = 'files_copy'
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.mkdir(newFolder, { recursive: true });
+        console.log(`Folder "${newFolder}" created (if not existing).`);
+    
+        const files = await fs.readdir(folderName);
+        await Promise.all(
+          files.map(async (file) => {
+            const sourcePath = path.join(folderName, file);
+            const targetPath = path.join(newFolder, file);
+            await fs.copyFile(sourcePath, targetPath);
+            console.log(`File "${file}" copied to "${newFolder}".`);
+          })
+        );
+      } catch (err) {
+        console.error('Error copying files:', err);
+      }
+    
 };
 
 await copy();
